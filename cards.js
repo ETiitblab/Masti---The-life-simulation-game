05a4575083d1522acaece6fcfc00643db8d5559c
Srujan <srujan_barai@yahoo.com>
2016-06-22 13:24:56 +0530

    Mid work commit (With errors, unreliable)

--- cards.js
+++ cards.js
@@ -1,5 +1,5 @@
 function showCard(){
-    // alert("from showcard");7
+    alert("showCard function was called.");
     document.getElementById("cardframe").style.display = "";
     document.getElementById("cardframe").src = "modal.html";
 }
